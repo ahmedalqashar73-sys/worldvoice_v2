f06c9e8e048518c30424f5045da1094b43158a0c
ahmedalqashar73-sys <ahmedalqashar73@gmail.com>
2026-09-21 14:05:54 +0300

    Add WorldVoice V2 design system foundation

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+abstract final class AppColors {
+  static const primary = Color(0xFF16A66A);
+  static const primaryDark = Color(0xFF118456);
+  static const sky = Color(0xFF38AEEA);
+  static const gold = Color(0xFFD6A62E);
+
+  static const lightBackground = Color(0xFFF7FAF9);
+  static const lightSurface = Color(0xFFFFFFFF);
+  static const lightText = Color(0xFF13201B);
+  static const lightMuted = Color(0xFF66736D);
+
+  static const darkBackground = Color(0xFF141A1D);
+  static const darkSurface = Color(0xFF1D2529);
+  static const darkText = Color(0xFFF2F6F4);
+  static const darkMuted = Color(0xFFAAB6B0);
+}
