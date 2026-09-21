e7097cec945cb090e1131a41fc6c820a1e5cb1ef
ahmedalqashar73-sys <ahmedalqashar73@gmail.com>
2026-09-21 14:13:30 +0300

    Wire persistent locale selection

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,9 +1,43 @@
 import 'package:flutter/material.dart';
 
+import '../core/localization/locale_controller.dart';
+import '../core/localization/supported_language.dart';
 import '../core/theme/app_theme.dart';
+import '../features/onboarding/presentation/language/language_selection_screen.dart';
 
-class WorldVoiceApp extends StatelessWidget {
+class WorldVoiceApp extends StatefulWidget {
   const WorldVoiceApp({super.key});
+
+  @override
+  State<WorldVoiceApp> createState() => _WorldVoiceAppState();
+}
+
+class _WorldVoiceAppState extends State<WorldVoiceApp> {
+  final LocaleController _localeController = LocaleController();
+  bool _ready = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _localeController.addListener(_refresh);
+    _load();
+  }
+
+  Future<void> _load() async {
+    await _localeController.load();
+    if (mounted) setState(() => _ready = true);
+  }
+
+  void _refresh() {
+    if (mounted) setState(() {});
+  }
+
+  @override
+  void dispose() {
+    _localeController.removeListener(_refresh);
+    _localeController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,43 +47,11 @@ class WorldVoiceApp extends StatelessWidget {
       theme: AppTheme.light,
       darkTheme: AppTheme.dark,
       themeMode: ThemeMode.system,
-      home: const _FoundationScreen(),
-    );
-  }
-}
-
-class _FoundationScreen extends StatelessWidget {
-  const _FoundationScreen();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(24),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Icon(Icons.public_rounded,
-                    size: 72, color: Theme.of(context).colorScheme.primary),
-                const SizedBox(height: 20),
-                Text(
-                  'WorldVoice',
-                  style: Theme.of(context).textTheme.headlineMedium?.copyWith(
-                        fontWeight: FontWeight.w800,
-                      ),
-                ),
-                const SizedBox(height: 8),
-                Text(
-                  'Speak. Connect. Learn.',
-                  style: Theme.of(context).textTheme.bodyLarge,
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
+      locale: _localeController.locale,
+      supportedLocales: SupportedLanguages.locales,
+      home: !_ready
+          ? const Scaffold(body: Center(child: CircularProgressIndicator()))
+          : LanguageSelectionScreen(controller: _localeController),
     );
   }
 }
