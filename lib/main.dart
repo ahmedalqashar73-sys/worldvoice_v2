947ce22a16ffa44fd3da00a18ec2b53e02b4682b
ahmedalqashar73-sys <ahmedalqashar73@gmail.com>
2026-09-24 00:59:11 +0300

    Initialize Google Mobile Ads for room rewarded ads

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 import 'app/app.dart';
 import 'firebase_options.dart';
@@ -10,6 +11,8 @@ Future<void> main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  await MobileAds.instance.initialize();
 
   runApp(const WorldVoiceApp());
 }
